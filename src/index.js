c802fb6452aa6177ef1c6932cf1591938cd09d63
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-05-13 18:03:29 +0700

    6:05 13/05/2023

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+// import path
+import * as path from "path";
+// import url
+import { fileURLToPath } from "url";
+
+const app = express();
+// set const __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Template engine
+app.engine("hbs", engine({
+  // change tall file name from handlebar to hbs
+  extname: ".hbs",
+}));
+
+// set location in views layouts
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen("3000");
