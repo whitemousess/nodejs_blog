84d38caefc84b2d28d0c2c891547b4006fdf8cd8
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-05-18 15:38:09 +0700

    query parameters

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Template engine
-app.engine("hbs", engine({
-  // change tall file name from handlebar to hbs
-  extname: ".hbs",
-}));
+app.engine(
+  "hbs",
+  engine({
+    // change tall file name from handlebar to hbs
+    extname: ".hbs",
+  })
+);
 
 // set location in views layouts
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -27,6 +29,11 @@ app.get("/", (req, res) => {
 
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  console.log(req.query.q);
+  res.render("search");
 });
 
 app.listen("3000");
