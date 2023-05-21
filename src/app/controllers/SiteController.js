2bb6ee4022975850a4d3ba0be3806f1161ce1147
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-05-21 18:26:59 +0700

    routes MVC controller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,17 @@
+// Controller Site
+
+class SiteController {
+
+    // [GET] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
+
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
-import express from "express";
-import { engine } from "express-handlebars";
-// import path
-import * as path from "path";
-// import url
-import { fileURLToPath } from "url";
-
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
-// set const __dirname
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine(
-  "hbs",
-  engine({
-    // change tall file name from handlebar to hbs
-    extname: ".hbs",
-  })
-);
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-// set location in views layouts
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
-app.use(express.static(path.join(__dirname, "public")));
+// Routes init
+route(app);
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  console.log(req.query.q);
-  res.render("search");
-});
-
-app.listen("3000");
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
