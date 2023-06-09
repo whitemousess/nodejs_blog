2aa656623e632812624071aaf01b0cffed450f2e
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-06-09 13:02:46 +0700

    6/9

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-    express.urlencoded({
+         express.urlencoded({
         extended: true,
     }),
 );
